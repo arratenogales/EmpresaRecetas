e7d652797246f8f13b3a1b594edd36cd90748e50
ArrateNogales <arrate.nogales@opendeusto.es>
2023-10-30 13:26:47 +0100

    Create urls.py

---
+++ empresaRecetas/appEmpresaRecetas/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('appEmpresaRecetas/', include('appEmpresaRecetas.urls')),
+    path('admin/', admin.site.urls),
+]
