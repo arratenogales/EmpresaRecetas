c7c4040764dbb9cf268a11ccf3b54a23620afe14
ArrateNogales <arrate.nogales@opendeusto.es>
2023-10-30 15:51:15 +0100

    modelo

--- empresaRecetas/appEmpresaRecetas/models.py
+++ empresaRecetas/appEmpresaRecetas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+ 
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=50)
+    
 
-# Create your models here.
+class TipoPlato(models.Model):
+    nombre = models.CharField(max_length=50)
+ 
+class Receta(models.Model):
+    tipo = models.ForeignKey(TipoPlato, on_delete=models.CASCADE)
+    #una receta tiene varios ingredientes)
+    ingredientes = models.ManyToManyField(Ingrediente)
+    nombre = models.CharField(max_length=40)
+    duracion = models.IntegerField()
