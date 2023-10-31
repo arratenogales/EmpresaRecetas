bdc9f39dddef4c812b2cdf068454b03727a11782
ArrateNogales <arrate.nogales@opendeusto.es>
2023-10-31 13:06:58 +0100

    Update urls.py

--- empresaRecetas/appEmpresaRecetas/urls.py
+++ empresaRecetas/appEmpresaRecetas/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index_recetas, name='index'),
+    path('receta/<int:receta_id>/', views.show_receta, name='detail'),
+    path('receta/<int:receta_id>/ingredientes', views.index_ingredientes, name='ingredientes'),
+    path('ingrediente/<int:ingrediente_id>', views.show_ingrediente, name='ingrediente'),
+    path('tipo/<int:tipo_id>', views.show_tipo, name='tipo'),
+
 ]
