e4bffac200acd150ac2bcd0f5700d8b2b464a518
aitziberlv <aitziber.l.v@opendeusto.es>
2023-10-31 11:39:34 +0100

    cambios

--- empresaRecetas/appEmpresaRecetas/models.py
+++ empresaRecetas/appEmpresaRecetas/models.py
@@ -2,10 +2,14 @@ from django.db import models
  
 class Ingrediente(models.Model):
     nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
     
 
 class TipoPlato(models.Model):
     nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
  
 class Receta(models.Model):
     tipo = models.ForeignKey(TipoPlato, on_delete=models.CASCADE)
@@ -13,3 +17,5 @@ class Receta(models.Model):
     ingredientes = models.ManyToManyField(Ingrediente)
     nombre = models.CharField(max_length=40)
     duracion = models.IntegerField()
+    def __str__(self):
+        return self.nombre
