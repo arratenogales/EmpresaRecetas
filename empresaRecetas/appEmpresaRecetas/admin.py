19335166ae773228d83164820ff095f913ec93cd
aitziberlv <aitziber.l.v@opendeusto.es>
2023-10-31 11:52:47 +0100

    añadir modelo a admin

--- empresaRecetas/appEmpresaRecetas/admin.py
+++ empresaRecetas/appEmpresaRecetas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Receta, Ingrediente, TipoPlato
+admin.site.register(Receta)
+admin.site.register(Ingrediente)
+admin.site.register(TipoPlato)
