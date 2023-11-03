a0ec8fa9fff87cef6af17576b5ea6d851c2aa48b
anesanjuan <ane.sanjuan@opendeusto.es>
2023-11-03 18:31:57 +0100

    new views

--- empresaRecetas/appEmpresaRecetas/urls.py
+++ empresaRecetas/appEmpresaRecetas/urls.py
@@ -3,9 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index_recetas, name='index'),
+    path('', views.index_ingredientes, name='index'),
+    path('', views.index_tipos, name='index'),
+
     path('receta/<int:receta_id>/', views.show_receta, name='detail'),
-    path('receta/<int:receta_id>/ingredientes', views.index_ingredientes, name='ingredientes'),
-    path('ingrediente/<int:ingrediente_id>', views.show_ingrediente, name='ingrediente'),
-    path('tipo/<int:tipo_id>', views.show_tipo, name='tipo'),
+    path('receta/<int:ingrediente_id>/', views.show_ingrediente, name='ingrediente'),
+    path('receta/<int:tipo_id>/', views.show_tipo, name='tipo'),
+    
+    #ingredientes de una receta
+    path('receta/<int:receta_id>/ingredientes', views.index_ingredientes_r, name='ingredientes receta'),
+    #recetas de un ingrediente
+    path('ingrediente/<int:ingrediente_id>/recetas', views.index_recetas_i, name='recetas ingrediente'),
+    #recetas de un tipo
+    path('tipo/<int:tipo_id>/recetas', views.index_recetas_t, name='recetas tipo')
 
 ]
--- empresaRecetas/appEmpresaRecetas/views.py
+++ empresaRecetas/appEmpresaRecetas/views.py
@@ -15,6 +15,20 @@ def index_recetas(request):
 	"""return HttpResponse(output)"""
 	return render(request, 'index.html', context)
 
+def index_ingredientes(request):
+	ingredientes = get_list_or_404(Ingrediente.objects.order_by('nombre'))
+	"""output = ', '.join([d.nombre for i in ingredientes])"""
+	context = {'lista_ingredientes': ingredientes }
+	"""return HttpResponse(output)"""
+	return render(request, 'ingredientes.html', context)
+
+def index_tipos(request):
+	tipos = get_list_or_404(TipoPlato.objects.order_by('nombre'))
+	"""output = ', '.join([d.nombre for t in tipos])"""
+	context = {'lista_tipos': tipos }
+	"""return HttpResponse(output)"""
+	return render(request, 'tipos.html', context)
+
 #devuelve los datos de una receta
 def show_receta(request, receta_id):
 	receta = get_list_or_404(Receta.objects.get(pk=receta_id))
@@ -23,30 +37,47 @@ def show_receta(request, receta_id):
 	context = {'receta': receta }
 	return render(request, 'receta.html', context)
 
-#devuelve los ingredientes de una receta
-def index_ingredientes(request, receta_id):
-	receta = get_list_or_404(Receta.objects.get(pk=receta_id))
-	ingredientes =  receta.ingrediente_set.all()
-	"""output = ', '.join([e.nombre for e in receta.ingredientes_set.all()])"""
-	"""return HttpResponse(output)"""
-	context = {'receta': receta, 'empleados' : ingredientes }
-	return render(request, 'ingredientes.html', context)
-
 #devuelve los detalles de un ingrediente
 def show_ingrediente(request, ingrediente_id):
 	ingrediente = get_list_or_404(Ingrediente.objects.get(pk=ingrediente_id))
 	recetas =  ingrediente.recetas_set.all()
 	context = { 'ingrediente': ingrediente, 'recetas' : recetas }
-	return render(request, 'ingrediente_receta.html', context) 
+	return render(request, 'ingrediente.html', context) 
 """	output = f'Detalles del ingrediente: {ingrediente.id}, {ingrediente.nombre}, {ingrediente.kcal}, {ingrediente.grasas}'"""
 """return HttpResponse(output)"""
-
 
 #devuelve los detalles de un tipo 
 def show_tipo(request, tipo_id):
 	tipo = get_list_or_404(TipoPlato.objects.get(pk=tipo_id))
 	recetas =  tipo.recetas_set.all()
 	context = { 'recetas': recetas, 'tipo' : tipo }
-	return render(request, 'tipo_recetas.html', context)  
+	return render(request, 'tipo.html', context)  
 """	output = f'Detalles de un tipo: {tipo.id}, {tipo.nombre}. Recetas: {[e.nombre for e in Receta.receta_set.all()]}'"""
 """return HttpResponse(output)"""
+
+#devuelve los ingredientes de una receta
+def index_ingredientes_r(request, receta_id):
+	receta = get_list_or_404(Receta.objects.get(pk=receta_id))
+	ingredientes =  receta.ingrediente_set.all()
+	"""output = ', '.join([e.nombre for e in receta.ingredientes_set.all()])"""
+	"""return HttpResponse(output)"""
+	context = {'receta': receta, 'ingredientes' : ingredientes }
+	return render(request, 'ingredientes_r.html', context)
+
+#devuelve las recetas de un tipo
+def index_recetas_t(request, tipo_id):
+	tipo = get_list_or_404(TipoPlato.objects.get(pk=tipo_id))
+	recetas =  tipo.receta_set.all()
+	"""output = ', '.join([e.nombre for e in tipo.recetas_set.all()])"""
+	"""return HttpResponse(output)"""
+	context = {'tipo': tipo, 'recetas' : recetas }
+	return render(request, 'recetas_t.html', context)
+
+#devuelve las recetas de un ingrediente
+def index_recetas_i(request, ingrediente_id):
+	ingrediente = get_list_or_404(Ingrediente.objects.get(pk=ingrediente_id))
+	recetas =  ingrediente.receta_set.all()
+	"""output = ', '.join([e.nombre for e in ingrediente.receta_set.all()])"""
+	"""return HttpResponse(output)"""
+	context = {'ingrediente': ingrediente, 'recetas' : recetas }
+	return render(request, 'recetas_i.html', context)
