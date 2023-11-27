5535a06b22398e3e849bc31b870eed462ccd808e
aitziberlv <aitziber.l.v@opendeusto.es>
2023-11-27 13:27:37 +0100

    cambios java

--- empresaRecetas/appEmpresaRecetas/static/js/js3.js
+++ empresaRecetas/appEmpresaRecetas/static/js/js3.js
@@ -1,3 +1,12 @@
+var thumbs = document.querySelectorAll('#thumbs img');
+  thumbs.forEach(function(thumb) {
+    thumb.addEventListener('click', function() {
+        alert("Bienvenido a nuestra web Gestor de Recetas");
+        document.getElementById('imagen-seleccionada').src = this.src;
+    });
+  });
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
     alert("Bienvenido a nuestra web Gestor de Recetas");
