4524cfa35245181efb10d4bd63916719bbbf0489
aitziberlv <aitziber.l.v@opendeusto.es>
2023-11-27 13:49:40 +0100

    cambio de java ya va bien falta ponerlo mejor

--- empresaRecetas/appEmpresaRecetas/static/js/js3.js
+++ empresaRecetas/appEmpresaRecetas/static/js/js3.js
@@ -1,13 +1,15 @@
-var thumbs = document.querySelectorAll('#thumbs img');
-  thumbs.forEach(function(thumb) {
-    thumb.addEventListener('click', function() {
-        alert("Bienvenido a nuestra web Gestor de Recetas");
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Bienvenido a nuestra web Gestor de Recetas");
+    var thumbs = document.querySelectorAll('#thumbs img');
+    thumbs.forEach(function(thumb) {
+      thumb.addEventListener('click', function() {
+        variable= document.getElementById('imagen-seleccionada').src;
         document.getElementById('imagen-seleccionada').src = this.src;
+        this.src=variable;
+      });
     });
   });
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Bienvenido a nuestra web Gestor de Recetas");
-});
+
