e851d3d81202dbd124aa2e467c80dfede1434e58
aitziberlv <aitziber.l.v@opendeusto.es>
2023-11-29 21:01:07 +0100

    me da un erroo el lunes le pregunto y lo arreglo

--- empresaRecetas/appEmpresaRecetas/static/js/js3.js
+++ empresaRecetas/appEmpresaRecetas/static/js/js3.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert("Bienvenido a nuestra web Gestor de Recetas");
+  alert("Bienvenido a nuestra web Gestor de Recetas");
 
-    var thumbs = document.querySelectorAll('#thumbs img');
+  var thumbs = document.querySelectorAll('#thumbs img');
 
-    var recetaElement = document.getElementById('receta');
-    
-    thumbs.forEach(function(thumb) {
-      thumb.addEventListener('click', function() {
-        variable= document.getElementById('imagen-seleccionada').src;
-        document.getElementById('imagen-seleccionada').src = this.src;
-        recetaElement.textContent = this.alt;
-        this.src=variable;
-        variable2= document.getElementById('imagen-seleccionada').alt;
-        document.getElementById('imagen-seleccionada').alt=this.src;
-        this.alt=variable2;
-        
-      });
+  var recetaElement = document.getElementById('receta');
+  
+  thumbs.forEach(function(thumb) {
+    thumb.addEventListener('click', function() {
+      variable= document.getElementById('imagen-seleccionada').src;
+      document.getElementById('imagen-seleccionada').src = this.src;
+      recetaElement.textContent = this.alt;
+      this.src=variable;
+      variable2= document.getElementById('imagen-seleccionada').alt;
+      document.getElementById('imagen-seleccionada').alt=this.src;
+      this.alt=variable2;
+      
     });
   });
+});
 
 
 
