fc055f505b00df7e06e077260bef5650c04928cc
aitziberlv <aitziber.l.v@opendeusto.es>
2023-11-30 17:01:07 +0100

    tenfo que arreglar tambien otra cosa de los comentarios

--- empresaRecetas/appEmpresaRecetas/static/js/indexjs.js
+++ empresaRecetas/appEmpresaRecetas/static/js/indexjs.js
@@ -35,10 +35,3 @@ function agregarComentario() {
   }
 
   
-  
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
