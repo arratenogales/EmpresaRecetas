9f38c10b20280fa28549ea711e619be2bdd8fd17
aitziberlv <aitziber.l.v@opendeusto.es>
2023-11-30 10:32:27 +0100

    intentar arreglar una cosa de un boton

--- empresaRecetas/appEmpresaRecetas/static/js/indexjs.js
+++ empresaRecetas/appEmpresaRecetas/static/js/indexjs.js
@@ -11,7 +11,20 @@ function agregarComentario() {
     nuevoComentario.innerHTML = '<strong>' + correo + ':</strong> ' + comentario +'<em> (Este comentario esta aun pendiente de revisar y aceptar por el sistema)</em> ';
 
     document.getElementById('comentarios').appendChild(nuevoComentario);
-
     document.getElementById('correo').value = '';
     document.getElementById('comentario').value = '';
+  }
+
+
+function toggleComentarios() {
+    var comentariosDiv = document.getElementById("comentarios2");
+    var boton = document.getElementById("comentarios2");
+
+    if (comentariosDiv.style.display === "none") {
+      comentariosDiv.style.display = "block";
+      boton.textContent = "Ocultar Comentarios";
+    } else {
+      comentariosDiv.style.display = "none";
+      boton.textContent = "Mostrar Comentarios";
+    }
   }
