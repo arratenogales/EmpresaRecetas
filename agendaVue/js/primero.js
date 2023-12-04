48e66ba135cf655d56d33512fff05e707892652a
ArrateNogales <arrate.nogales@opendeusto.es>
2023-12-04 16:46:25 +0100

    no se porque no se añade el nuevo contacto

---
+++ agendaVue/js/primero.js
@@ -0,0 +1,47 @@
+var appVue = new Vue({
+    el: '#app-1',
+    data: {
+      state: 'default',
+      header: 'AgendaVue',
+      nuevoContacto: {
+        name: '',
+        email: '',
+        phone: ''
+      },
+      contactos: [
+        {
+          name: 'Arrate Nogales',
+          email: 'arrate@gmail.com',
+          phone: '678-456-213'
+        }
+      ] 
+    },
+    computed: {
+      contactCount() {
+        return this.contactos.length;
+      }
+    },
+    methods: {
+     aniadirContacto: function (contact) {
+        console.log('Añadiendo contacto:', contact);
+
+        this.contactos.push({
+          name: contact.name,
+          email: contact.email,
+          phone: contact.phone
+        });
+        this.nuevoContacto = { name: '', email: '', phone: '' };
+      },
+      changeState: function (newState) {
+        this.state = newState;
+      },
+
+      removeContact: function(contact) {
+        const index = this.contactos.indexOf(contact);
+        if (index !== -1) {
+          this.contactos.splice(index, 1);
+        }      
+      }
+    }
+  });
+  
